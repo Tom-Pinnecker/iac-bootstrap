eced9fb52954b2ef4fbb1f8a905fa353c98da249
Tom Pinnecker <info@pinnecker.io>
2026-07-02 13:11:33 +0200

    Adding docker installation step

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ set -eu
 # Bootstrap Ubuntu Server
 #
 # - Updates system
-# - Installs Git
+# - Installs Git + Docker
 # - Creates GitHub deploy key
 # - Configures SSH
 # - Creates /srv directory structure
@@ -70,7 +70,45 @@ log_info "Installing packages"
 
 apt install -y \
     git \
-    ca-certificates
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+###############################################################################
+# Install docker
+###############################################################################
+
+log_info "Installing Docker dependencies..."
+
+log_info "Adding Docker GPG key..."
+
+install -m 0755 -d /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
+    | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+log_info "Adding Docker repository..."
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" \
+  > /etc/apt/sources.list.d/docker.list
+
+log_info "Installing Docker Engine..."
+
+apt update
+
+apt install -y \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io \
+    docker-buildx-plugin \
+    docker-compose-plugin
+
+log_ok "Docker installed"
 
 ###############################################################################
 # SSH
